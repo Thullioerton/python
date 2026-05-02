62840e5762f41a66e7a4be532e4407a505a2c5eb
Thullioerton <thulliononato@gmail.com>
2026-05-02 20:29:56 -0300

    adicionando o codigo

---
+++ main.py
@@ -0,0 +1,22 @@
+# Sintaxe 🧱
+
+# Sintaxe é como escrever o código corretamente.
+
+# Ex: 
+
+print("Oi") / print Oi
+
+
+# Variáveis
+
+# Pense assim: é uma “caixinha” 📦
+
+nome = "Ana"
+idade = 13
+
+# 3. Tipos de dados (o que você guarda) 🔤
+
+"texto"   # str
+10        # int
+3.5       # float
+True      # bool
